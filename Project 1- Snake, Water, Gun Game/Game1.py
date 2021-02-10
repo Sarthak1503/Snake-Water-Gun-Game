157c67454c716df06b0db4dfb6b39f296e13d151
Faizan <coder98321@gmail.com>
2021-02-10 11:42:42 +0530

    Made the code better!

--- Project 1- Snake, Water, Gun Game/Game1.py
+++ Project 1- Snake, Water, Gun Game/Game1.py
@@ -3,6 +3,7 @@ import random
 #Snake Water Gun or Rock Paper Scissors
 def gameWin(comp, you):
 
+
     #if two values are equal, declare a tie!
     if comp == you:
         return None
@@ -25,7 +26,8 @@ def gameWin(comp, you):
         elif you == 'w':
             return True
 
-print("Comp Turn: Snake(s) Water(w) Gun(g)?") 
+name = input("\nEnter your name here: ")
+print("\nComp Turn: Snake(s) Water(w) Gun(g)? ") 
 randNo = random.randint(1,3)
 if randNo == 1:
     comp = 's'
@@ -33,16 +35,16 @@ elif randNo == 2:
     comp = 'w'
 elif randNo == 3:
     comp = 'g'
-    
-you = input("Your Turn: Snake(s) Water(w) Gun(g)?") 
+you = input(f"\n{name}'s Turn: Snake(s) Water(w) Gun(g)? ") 
 a= gameWin(comp, you)
 
-print(f"Computer chose{comp}")
-print(f"You chose{you}")
+print(f"Computer chose {comp}")
+print(f"{name} chose {you}")
+
 
 if a == None:
-    print("The game is a tie!")
-elif a:
-    print("You win!")
+    print("\nThe game is a tie!")
+elif a == True:
+    print("\nYou win!")
 else:
-    print("You lose!")
+    print("\nYou lose!")
